d26ada545dd6b0920e06c00ece2fa81c445fc824
S-Camille <camille54460@gmail.com>
2019-03-20 13:04:40 +0100

    Modif script bdd 2

--- geoquizz_data.sql
+++ geoquizz_data.sql
@@ -15,3 +15,19 @@ INSERT INTO photo (id, descr, latitude, longitude, url, serie_id) VALUES
 ('0cc0c744-679a-4767-953b-aa72cb8531f0', 'Gare de Nancy', '48.689841', '6.174445', 'http://res.cloudinary.com/lesporos/image/upload/0cc0c744-679a-4767-953b-aa72cb8531f0.jpg', 'e084872b-6328-46af-97fc-6cb909bf4f72');
 
 INSERT INTO serie (id, ville, map_lat, map_lon) VALUES ('418ee498-85f9-4680-b47e-87bf7ecca3a0', 'Londres', '51.489334', '-0.144055');
+
+INSERT INTO photo (id, descr, latitude, longitude, url, serie_id) VALUES
+('71545b71-f6f7-40ad-aed6-ea2f12e52e62', 'Big Ben', '51.5007', '-0.124584', 'http://res.cloudinary.com/lesporos/image/upload/71545b71-f6f7-40ad-aed6-ea2f12e52e62.jpg', '418ee498-85f9-4680-b47e-87bf7ecca3a0'),
+('3fe4b85c-6a22-4ca3-9a8e-0ac8f056c996', 'London Eye', '51.5033', '-0.119686', 'http://res.cloudinary.com/lesporos/image/upload/d3fe4b85c-6a22-4ca3-9a8e-0ac8f056c996.jpg', '418ee498-85f9-4680-b47e-87bf7ecca3a0'),
+('54e73cdd-ce9c-41dd-b135-8d3cfc4a3d9b', 'Palais de Buckingham', '51.50137', '-0.1419', 'http://res.cloudinary.com/lesporos/image/upload/54e73cdd-ce9c-41dd-b135-8d3cfc4a3d9b.jpg', '418ee498-85f9-4680-b47e-87bf7ecca3a0'),
+('5307e2b2-3d57-4441-ad97-8cd8a82f9f58', 'Tower Bridge', '51.5055', '-0.075358', 'http://res.cloudinary.com/lesporos/image/upload/5307e2b2-3d57-4441-ad97-8cd8a82f9f58.jpg', '418ee498-85f9-4680-b47e-87bf7ecca3a0'),
+('d96cba1d-347d-4c93-8872-b0cd1e3e0735', 'Abbaye de Westminster', '51.499293', '-0.127319', 'http://res.cloudinary.com/lesporos/image/upload/d96cba1d-347d-4c93-8872-b0cd1e3e0735.jpg', '418ee498-85f9-4680-b47e-87bf7ecca3a0'),
+('a885cf4e-ff4a-4787-98a5-20db6225224c', 'Palais de Westminster', '51.49974', '-0.124648', 'http://res.cloudinary.com/lesporos/image/upload/a885cf4e-ff4a-4787-98a5-20db6225224c.jpg', '418ee498-85f9-4680-b47e-87bf7ecca3a0'),
+('68ef0dda-7964-4a51-a13b-5f84aed0bd2b', 'Trafalgar Square', '51.50804', '-0.12807', 'http://res.cloudinary.com/lesporos/image/upload/68ef0dda-7964-4a51-a13b-5f84aed0bd2b.jpg', '418ee498-85f9-4680-b47e-87bf7ecca3a0'),
+('4e0cef5c-494d-44a8-85cf-7e604aa88c88', 'Camden Town', '51.541511', '-0.145711', 'http://res.cloudinary.com/lesporos/image/upload/4e0cef5c-494d-44a8-85cf-7e604aa88c88.jpg', '418ee498-85f9-4680-b47e-87bf7ecca3a0'),
+('d5e018a5-ece4-4782-8624-34d139d121b8', 'Musée d histoire naturelle de Londres', '51.496334', '-0.176466', 'http://res.cloudinary.com/lesporos/image/upload/d5e018a5-ece4-4782-8624-34d139d121b8.jpg', '418ee498-85f9-4680-b47e-87bf7ecca3a0'),
+('918f5dcb-0829-48a9-862e-6d4470dab846', 'National Gallery', '51.50883', '-0.128413', 'http://res.cloudinary.com/lesporos/image/upload/918f5dcb-0829-48a9-862e-6d4470dab846.jpg', '418ee498-85f9-4680-b47e-87bf7ecca3a0'),
+('11caf53f-7ca7-4d85-a5f3-b67ddb411aab', 'The Shard', '51.504342', '-0.086446', 'http://res.cloudinary.com/lesporos/image/upload/11caf53f-7ca7-4d85-a5f3-b67ddb411aab.jpg', '418ee498-85f9-4680-b47e-87bf7ecca3a0'),
+('616b861b-3294-43ea-a759-26151846a8d2', 'Picadilly Circus', '51.5099', '-0.134261', 'http://res.cloudinary.com/lesporos/image/upload/616b861b-3294-43ea-a759-26151846a8d2.jpg', '418ee498-85f9-4680-b47e-87bf7ecca3a0');
+
+INSERT INTO user (username, password) VALUES ('svz', '');
